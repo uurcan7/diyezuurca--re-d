56f4ea171837c8c6fcc8e8c97234cd66e50ef258
ÇETİN, Uğurcan <uurcan70@gmail.com>
2022-07-30 00:12:09 +0300

    Delete files directory

--- files/clerr.js
+++
@@ -1,3 +0,0 @@
-console.log('%c uurcan7', 'color: #ffc0cb; font-size: 48px;');
-console.log('%c Bir şeye mi ihtiyacın var 🤔', 'color: #ff69b4; font-size: 24px;');
-console.log('%c Garip bir hata.. Bize bildirmeye ne dersin? :) mailto:info@diyezuurca.com', 'color: #c0b691; font-size: 12px;');
